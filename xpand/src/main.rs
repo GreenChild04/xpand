61e44a5bfac397a7dc5338632ed5118603b7b60e
GreenChild04 <greenchild04@protonmail.com>
2023-12-06 20:47:55 +1100

    added splitting files into segments

--- xpand/src/main.rs
+++ xpand/src/main.rs
@@ -3,7 +3,6 @@ use serenity::{Client, model::gateway::GatewayIntents};
 
 #[tokio::main]
 async fn main() {
-    let value = [0u8; 4194304];
     let mut client = Client::builder(secrets::TOKEN.trim(), GatewayIntents::all())
         .event_handler(Bot)
         .application_id(secrets::APP_ID)
---
+++ xpand/src/segment.rs
@@ -0,0 +1,28 @@
+use std::{path::Path, fs::File, io::{self, BufReader, Read, Write, BufWriter}};
+
+/// Segment the file into 24-25MB smaller files
+pub fn segment(source_file: impl AsRef<Path>, out_path: impl AsRef<Path>) -> Result<(), io::Error> {
+    let mut source_file = BufReader::new(File::open(source_file)?);
+    
+    let mut i = 0u16;
+    let mut out_file = BufWriter::new(File::create(out_path.as_ref().join(i.to_string()))?);
+    let mut buffer = vec![0u8; 4 * 1024 * 1024].into_boxed_slice(); // 4MiB buffer
+    let mut bytes_read = source_file.read(&mut *buffer)?;
+    let mut current_size: usize = 0;
+
+    while bytes_read != 0 {
+        current_size += 4194304; // add 1MiB to current segment size
+        out_file.write_all(&buffer[0..bytes_read])?; // write segment's contents to out_file
+
+        if current_size >= 25000000 { // if current segment larger than or equal to 25MB them move onto the next segment
+            i += 1;
+            current_size = 0;
+            out_file.flush()?;
+            out_file = BufWriter::new(File::create(out_path.as_ref().join(i.to_string()))?);
+        }
+        
+        bytes_read = source_file.read(&mut *buffer)?;
+    }
+
+    Ok(())
+}
