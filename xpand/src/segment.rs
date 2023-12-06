818b230839ab275f2c50976674955eb4859fb9fd
GreenChild04 <greenchild04@protonmail.com>
2023-12-07 09:54:35 +1100

    fixed segments being too large

--- xpand/src/segment.rs
+++ xpand/src/segment.rs
@@ -11,15 +11,13 @@ pub fn segment(source_file: impl AsRef<Path>, out_path: impl AsRef<Path>) -> Res
     let mut current_size: usize = 0;
 
     while bytes_read != 0 {
-        current_size += 4194304; // add 1MiB to current segment size
-        out_file.write_all(&buffer[0..bytes_read])?; // write segment's contents to out_file
-
+        current_size += bytes_read; // add read size to
         if current_size >= 25000000 { // if current segment larger than or equal to 25MB them move onto the next segment
             i += 1;
-            current_size = 0;
+            current_size = bytes_read;
             out_file.flush()?;
             out_file = BufWriter::new(File::create(out_path.as_ref().join(i.to_string()))?);
-        }
+        } out_file.write_all(&buffer[0..bytes_read])?; // write segment's contents to out_file
         
         bytes_read = source_file.read(&mut *buffer)?;
     }
