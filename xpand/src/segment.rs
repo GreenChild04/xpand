40dea57cc9f9a78f121adcdf6540b2bb146d897e
GreenChild04 <greenchild04@protonmail.com>
2023-12-09 18:35:27 +1100

    almost forgot to add code to upload the segment ids

--- xpand/src/segment.rs
+++ xpand/src/segment.rs
@@ -15,25 +15,24 @@ pub async fn segment_upload(source_file: impl AsRef<Path>, channel_id: u64, ctx:
 
     loop {
         let bytes_read = fill_buffer(&mut buffer, &mut source_file).await?;
-        println!("Read {} bytes", bytes_read);
         if bytes_read == 0 { break }; // if no bytes read then break
         
-        // hash and upload segment
-        let hash = crypto::hash(&buffer[0..bytes_read]);
-        let attachment = CreateAttachment::bytes(&buffer[0..bytes_read], i.to_string());
-        let message = CreateMessage::default()
-            .content(base64::engine::general_purpose::URL_SAFE.encode(hash));
-        let message = channel_id.send_files(&ctx.http, [attachment], message)
+        // hash and upload segment and add it to the list of segment ids
+        segment_ids.push(upload_bytes(&buffer[..bytes_read], &format!("segment_{}", i), &channel_id, ctx)
             .await
-            .expect("Error uploading file");
-
-        segment_ids.push(message.id.get());
+            .expect("Error uploading segment"));
         i += 1;
     }
 
-    Ok(0)
+    Ok(upload_bytes(
+        &bincode::serialize(&segment_ids).expect("Error serializing segment ids"),
+        "segment_ids",
+        &channel_id,
+        ctx
+    ).await.expect("Error uploading segment ids"))
 }
 
+/// Fills the buffer with data from the file
 #[inline]
 async fn fill_buffer(buffer: &mut [u8], file: &mut File) -> Result<usize, io::Error> {
     let mut i = 0;
@@ -44,4 +43,15 @@ async fn fill_buffer(buffer: &mut [u8], file: &mut File) -> Result<usize, io::Er
     }
 
     Ok(i)
+}
+
+/// Uploads bytes to the server
+#[inline]
+async fn upload_bytes(bytes: &[u8], file_name: &str, channel_id: &ChannelId, ctx: &Context) -> Result<u64, serenity::prelude::SerenityError> {
+    let attachment = CreateAttachment::bytes(bytes, file_name);
+    let message = CreateMessage::default()
+        .content(base64::engine::general_purpose::URL_SAFE.encode(crypto::hash(bytes)));
+    let message = channel_id.send_files(&ctx.http, [attachment], message).await?;
+
+    Ok(message.id.get())
 }
